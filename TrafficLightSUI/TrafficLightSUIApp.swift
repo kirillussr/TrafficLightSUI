c7ff387f5bcef6a54ae21fdb201d2dcc8346e3b4
Kirill <133357229+kirillussr@users.noreply.github.com>
2023-09-11 17:42:58 +0300

    Initial Commit

---
+++ TrafficLightSUI/TrafficLightSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSUIApp.swift
+//  TrafficLightSUI
+//
+//  Created by Кирилл on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
